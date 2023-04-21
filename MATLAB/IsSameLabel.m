82479b4882b6c611b5c763891ee39ec6e3bbe81a
sxyupc <115883140+sxyupc@users.noreply.github.com>
2023-04-21 16:25:10 +0800

    Delete MATLAB directory

--- MATLAB/IsSameLabel.m
+++
@@ -1,7 +0,0 @@
-function Coef = IsSameLabel(Lab, MinDisLab)
-if MinDisLab == Lab
-   Coef = 1;
-else
-   Coef = 0;
-end
-end
--- MATLAB/SVMTrain.m
+++
@@ -1,6 +0,0 @@
-function [Mdl] = SVMTrain(TrainData,TrainLabel,UniqueLabel)
-%SVM train
-T = templateSVM('Standardize',1,'KernelFunction','gaussian');
-Mdl = fitcecoc(TrainData,TrainLabel,'Learners',T,'Coding','onevsone','FitPosterior',1,...
-    'ClassNames',UniqueLabel,...
-    'Verbose',2);
